a08c57da73788d6ec9d3d2738067121cd6f5ba73
William Mayen <willysomv101@gmail.com>
2022-02-07 12:53:55 -0600

    Cursos

--- tarea2/clasesNotas.cpp
+++ tarea2/clasesNotas.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
 int main()
 {
+    cout <<"Cursos Asignados"<< endl;
+
+    cout <<"9959 011 Fisica I"<< endl;
+    cout <<"9959 012 Programacion I"<< endl;
+    cout <<"9959 013 Calculo I"<< endl;
+    cout <<"9959 014 Proceso Administrativo"<< endl;
+    cout <<"9959 015 Derecho Informatico"<< endl;
+
+    string CodigoCurso;
+    cout <<"Ingrese codigo de curso seguido del '9959' para ingresar nota al curso" << endl;
+    cin >> CodigoCurso;
+
+
+}
